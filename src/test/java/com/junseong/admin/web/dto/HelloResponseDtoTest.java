71e27acb7744beacf4965d81da35b07716caa04a
김준성 <anima94@naver.com>
2020-07-13 22:29:37 +0900

    gradle downgrade + add lombok dependency :+1:

---
+++ src/test/java/com/junseong/admin/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.junseong.admin.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트(){
+        String name="test";
+        int amount=1000;
+
+        HelloResponseDto dto=new HelloResponseDto(name,amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
