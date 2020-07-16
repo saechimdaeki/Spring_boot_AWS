e22faf610379c4f59241061e5012add68faf7b3c
김준성 <anima94@naver.com>
2020-07-16 17:01:03 +0900

    fix&feature) until google login :goat:

--- src/main/java/com/junseong/admin/web/IndexController.java
+++ src/main/java/com/junseong/admin/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.junseong.admin.web;
 
+import com.junseong.admin.config.auth.LoginUser;
+import com.junseong.admin.config.auth.dto.SessionUser;
 import com.junseong.admin.service.PostsService;
 import com.junseong.admin.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,13 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user!=null)
+            model.addAttribute("userName",user.getName());
         return "index";
     }
 
