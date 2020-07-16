72a5c663338af4dcb027d170cf5da97b89fd962f
김준성 <anima94@naver.com>
2020-07-16 17:02:58 +0900

    error fix at Window OS

--- src/main/java/com/junseong/admin/web/IndexController.java
+++ src/main/java/com/junseong/admin/web/IndexController.java
@@ -20,7 +20,7 @@ public class IndexController {
     public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
         if(user!=null)
-            model.addAttribute("userName",user.getName());
+            model.addAttribute("loginusername",user.getName());
         return "index";
     }
 
